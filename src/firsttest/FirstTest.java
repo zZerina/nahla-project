ab5ef2f67028de0b5ccc36b9eb57a7eda7700028
zerina <zerina.t13@gmail.com>
2020-06-10 18:59:50 +0200

    NoviProjekat

---
+++ src/firsttest/FirstTest.java
@@ -0,0 +1,13 @@
+package firsttest;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		
+		// print in console some text
+		
+		System.out.print("Dobrodosli u nahlu");
+
+	}
+
+}
---
+++ src/secondtest/SecondTest.java
@@ -0,0 +1,24 @@
+package secondtest;
+
+public class SecondTest {
+
+	public static void main(String[] args) {
+		
+		
+		//declare variable
+		
+		int BrojStudenata =0;
+		
+		if(BrojStudenata>10) {
+			
+			System.out.println ("Vecina ucenika je dosla. Cas moze poceti.");
+			
+	
+		} else {
+			
+			System.out.println ("Nedovoljan broj ucenika za odrzavanje.");
+		}
+
+	}
+
+}
